dcdd03e9ba2842fad204262f1506542a1a500b7f
Jesus <ruizfloresadrianalejandro@gmail.com>
2025-10-31 14:51:23 -0400

    Add files via upload

---
+++ jokers/alt_account.lua
@@ -0,0 +1,81 @@
+SMODS.Joker{ --Alt Account
+    key = "alt_account",
+    config = {
+        extra = {
+            perishable = 0,
+            ignore = 0
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Alt Account',
+        ['text'] = {
+            [1] = 'When a {C:attention}Boss Blind{} is defeated',
+            [2] = 'create a {C:dark_edition}Negative{} {C:blue}Perishable{}',
+            [3] = 'copy of the {C:attention}Joker{} to the right'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 2,
+        y = 1
+    },
+    display_size = {
+        w = 71 * 0.95, 
+        h = 95 * 0.29
+    },
+    cost = 5,
+    rarity = "discord_message",
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    pools = { ["discord_dm_me"] = true },
+
+    loc_vars = function(self, info_queue, card)
+        
+        local info_queue_0 = G.P_CENTERS["e_negative"]
+        if info_queue_0 then
+            info_queue[#info_queue + 1] = info_queue_0
+        else
+            error("JOKERFORGE: Invalid key in infoQueues. \"e_negative\" isn't a valid Object key, Did you misspell it or forgot a modprefix?")
+        end
+        return {vars = {}}
+    end,
+
+    
+    calculate = function(self, card, context)
+    if context.end_of_round and context.main_eval and G.GAME.blind.boss  and not context.blueprint then
+        return {
+            func = function()
+                local my_pos = nil
+                for i = 1, #G.jokers.cards do
+                    if G.jokers.cards[i] == card then
+                        my_pos = i
+                        break
+                    end
+                end
+                local target_joker = (my_pos and my_pos < #G.jokers.cards) and G.jokers.cards[my_pos + 1] or nil
+                
+                if target_joker then
+                    G.E_MANAGER:add_event(Event({
+                    func = function()
+                        local copied_joker = copy_card(target_joker, nil, nil, nil, target_joker.edition and target_joker.edition.negative)
+                        copied_joker:set_edition("e_negative", true)
+                        copied_joker:add_sticker('perishable', true)
+                        copied_joker:add_to_deck()
+                        G.jokers:emplace(copied_joker)
+                        return true
+                        end
+                    }))
+                    card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_duplicated_ex'), colour = G.C.GREEN})
+                end
+                return true
+                end
+            }
+        end
+    end
+}
---
+++ jokers/waffle.lua
@@ -0,0 +1,78 @@
+SMODS.Joker{ --Waffle?
+    key = "waffle",
+    config = {
+        extra = {
+            respect = 0
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Waffle?',
+        ['text'] = {
+            [1] = 'Creates {C:attention}Happy Tomato{} when',
+            [2] = 'a {C:attention}Boss Blind{} is defeated',
+            [3] = '{C:inactive}(Must have room){}'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 6,
+        y = 3
+    },
+    display_size = {
+        w = 71 * 0.95, 
+        h = 95 * 0.53
+    },
+    cost = 5,
+    rarity = "discord_message",
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    pools = { ["discord_dm_me"] = true },
+
+    loc_vars = function(self, info_queue, card)
+        
+        local info_queue_0 = G.P_CENTERS["j_discord_happy_tomato"]
+        if info_queue_0 then
+            info_queue[#info_queue + 1] = info_queue_0
+        else
+            error("JOKERFORGE: Invalid key in infoQueues. \"j_discord_happy_tomato\" isn't a valid Object key, Did you misspell it or forgot a modprefix?")
+        end
+        return {vars = {}}
+    end,
+
+    
+    calculate = function(self, card, context)
+    if context.end_of_round and context.main_eval and G.GAME.blind.boss  then
+        return {
+            func = function()
+                
+                local created_joker = false
+                if #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit then
+                    created_joker = true
+                    G.GAME.joker_buffer = G.GAME.joker_buffer + 1
+                    G.E_MANAGER:add_event(Event({
+                    func = function()
+                        local joker_card = SMODS.add_card({ set = 'Joker', key = 'j_discord_happytomato' })
+                        if joker_card then
+                            
+                            
+                        end
+                        G.GAME.joker_buffer = 0
+                        return true
+                        end
+                    }))
+                end
+                if created_joker then
+                    card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_plus_joker'), colour = G.C.BLUE})
+                end
+                return true
+                end
+            }
+        end
+    end
+}
