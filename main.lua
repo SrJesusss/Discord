ac7679f737b5acca24bc0a2a25e5a5f3a919aaba
Jesus <ruizfloresadrianalejandro@gmail.com>
2025-12-20 16:26:42 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,156 @@
+SMODS.Atlas({
+    key = "modicon", 
+    path = "ModIcon.png", 
+    px = 34,
+    py = 34,
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "balatro", 
+    path = "balatro.png", 
+    px = 333,
+    py = 216,
+    prefix_config = { key = false },
+    atlas_table = "ASSET_ATLAS"
+})
+
+
+SMODS.Atlas({
+    key = "CustomJokers", 
+    path = "CustomJokers.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "CustomDecks", 
+    path = "CustomDecks.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+local NFS = require("nativefs")
+to_big = to_big or function(a) return a end
+lenient_bignum = lenient_bignum or function(a) return a end
+
+local jokerIndexList = {27,38,17,11,35,30,12,48,19,34,21,10,23,26,25,32,24,15,3,47,46,39,29,45,4,20,8,9,2,6,22,5,36,41,1,14,18,50,13,42,40,16,33,49,43,44,28,37,7,31}
+
+local function load_jokers_folder()
+    local mod_path = SMODS.current_mod.path
+    local jokers_path = mod_path .. "/jokers"
+    local files = NFS.getDirectoryItemsInfo(jokers_path)
+    for i = 1, #jokerIndexList do
+        local file_name = files[jokerIndexList[i]].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("jokers/" .. file_name))()
+        end
+    end
+end
+
+
+local deckIndexList = {1}
+
+local function load_decks_folder()
+    local mod_path = SMODS.current_mod.path
+    local decks_path = mod_path .. "/decks"
+    local files = NFS.getDirectoryItemsInfo(decks_path)
+    for i = 1, #deckIndexList do
+        local file_name = files[deckIndexList[i]].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("decks/" .. file_name))()
+        end
+    end
+end
+
+local function load_rarities_file()
+    local mod_path = SMODS.current_mod.path
+    assert(SMODS.load_file("rarities.lua"))()
+end
+
+load_rarities_file()
+load_jokers_folder()
+load_decks_folder()
+SMODS.ObjectType({
+    key = "discord_food",
+    cards = {
+        ["j_gros_michel"] = true,
+        ["j_egg"] = true,
+        ["j_ice_cream"] = true,
+        ["j_cavendish"] = true,
+        ["j_turtle_bean"] = true,
+        ["j_diet_cola"] = true,
+        ["j_popcorn"] = true,
+        ["j_ramen"] = true,
+        ["j_selzer"] = true,
+        ["j_discord_chicken_sandwich"] = true,
+        ["j_discord_hacked_user"] = true,
+        ["j_discord_happy_tomato"] = true,
+        ["j_discord_savings"] = true
+    },
+})
+
+SMODS.ObjectType({
+    key = "discord_dm_me",
+    cards = {
+        ["j_discord_abelsketch"] = true,
+        ["j_discord_agente_spia_cavo"] = true,
+        ["j_discord_alt_account"] = true,
+        ["j_discord_automod"] = true,
+        ["j_discord_blue"] = true,
+        ["j_discord_bonethy"] = true,
+        ["j_discord_cassknows"] = true,
+        ["j_discord_cavendish"] = true,
+        ["j_discord_cavo"] = true,
+        ["j_discord_chicken_sandwich"] = true,
+        ["j_discord_clown"] = true,
+        ["j_discord_clowning_user"] = true,
+        ["j_discord_comykel"] = true,
+        ["j_discord_doggfly"] = true,
+        ["j_discord_emoticon"] = true,
+        ["j_discord_fercorn"] = true,
+        ["j_discord_fully_grown_user"] = true,
+        ["j_discord_glitchkat10"] = true,
+        ["j_discord_hacked_user"] = true,
+        ["j_discord_half_a_dima"] = true,
+        ["j_discord_happy_tomato"] = true,
+        ["j_discord_kassandra"] = true,
+        ["j_discord_liar"] = true,
+        ["j_discord_lucky_catch"] = true,
+        ["j_discord_minecraft_player"] = true,
+        ["j_discord_moderator"] = true,
+        ["j_discord_nitro_basic"] = true,
+        ["j_discord_patitofi8"] = true,
+        ["j_discord_potential_dangerous_download"] = true,
+        ["j_discord_ragebaiter"] = true,
+        ["j_discord_red_theme"] = true,
+        ["j_discord_roleplayer"] = true,
+        ["j_discord_saucequest31"] = true,
+        ["j_discord_savings"] = true,
+        ["j_discord_self_reaction_spoted"] = true,
+        ["j_discord_sillyz_jimbo"] = true,
+        ["j_discord_snow_pickle"] = true,
+        ["j_discord_spammer"] = true,
+        ["j_discord_spoiler_message"] = true,
+        ["j_discord_spookyrazz"] = true,
+        ["j_discord_ssarahh"] = true,
+        ["j_discord_stefwuzhere"] = true,
+        ["j_discord_superbread"] = true,
+        ["j_discord_the_wheel"] = true,
+        ["j_discord_the_wheel_of_misfortune"] = true,
+        ["j_discord_totally_rigged"] = true,
+        ["j_discord_underage_user"] = true,
+        ["j_discord_very_scary_jumpscare"] = true,
+        ["j_discord_waffle"] = true,
+        ["j_discord_zombieboy139"] = true
+    },
+})
+
+
+SMODS.current_mod.optional_features = function()
+    return {
+        cardareas = {} 
+    }
+end
